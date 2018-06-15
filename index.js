9f67e9f4d202584c21f1971f0ae9d682a1681262
ArvinLleva <arvin.lleva@yahoo.com>
2018-06-15 20:18:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object 
 }
 
+<<<<<<< HEAD
 function deleteFromObjectByKey(object, key){
   var objectCopy = Object.assign({},object) // this creates a clone 
   delete objectCopy[key] // delets the 'key' from the clone
@@ -18,4 +19,10 @@ function deleteFromObjectByKey(object, key){
 function destructivelyDeleteFromObjectByKey(object, key){
   delete object[key]
   return object
+=======
+function deleteFromObjectsByKey(object, key){
+  var objectCopy = Object,assign({},object)// this creates a clone 
+  delete objectCopy[key] // delets the 'key' from the clone
+  return objectCopy // returns the clone
+>>>>>>> ab69eefb3d6ded2ab4b956b55c718a28be0c74dc
 }
